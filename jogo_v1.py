504d525f583a4e8fac8004acc41a7c60dab8d04d
Pedro Ivo <pedroir@al.inser.edu.br>
2021-05-19 18:36:29 -0300

    adicionando movimentos

--- jogo_v1.py
+++ jogo_v1.py
@@ -4,10 +4,21 @@ pygame.init()
 
 WIDTH = 500
 HEIGHT = 400
+ball_width = 30
+ball_height = 20
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Goalkeeper pro')
 background = pygame.image.load('img/estádio.png').convert()
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+football = pygame.image.load('img/football.png').convert()
+football = pygame.transform.scale(football, (ball_width, ball_height))
+
+
+football_x = 250
+football_y = -ball_height
+football_speedx = 4
+football_speedy = 8
+
 
 game = True
 while game:
@@ -15,8 +26,17 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+    football_x+=football_speedx
+    football_y+=football_speedy
+    if football_x > WIDTH or football_y > HEIGHT:
+        football_x = 250
+        football_y = -ball_height
+
+        
+
     window.fill((255, 255, 255))
     window.blit(background, (0, 0))
+    window.blit(football, (football_x, football_y))
 
     pygame.display.update()
 
