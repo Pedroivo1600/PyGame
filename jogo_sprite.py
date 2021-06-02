472fa658243c6881248282b389c4baddfe464336
lucasulson <lucasnu@al.insper.edu.br>
2021-06-02 10:15:02 -0300

    funçao

--- jogo_sprite.py
+++ jogo_sprite.py
@@ -10,13 +10,13 @@ pygame.init()
 list_lives = [] #lista de vidas
 shield = False #boolean que indica se o powerup "Shield" está aparecendo no jogo ou não
 p_up = False #boolean que indica se o jogador colidiu com o  powerup no jogo ou não
-
+FPS = 30
 #MEDIDAS
 #Medidas do background (estadio)
 WIDTH = 500 # largura da tela
 HEIGHT = 400 # altura da tela
 window = pygame.display.set_mode((WIDTH, HEIGHT)) #cria a tela do jogo
- 
+
 #Medidas das bolas
 ball_width = 30 #largura da bola
 ball_height = 20 # altura da bola
@@ -27,18 +27,19 @@ gk_height = 60 # altura do goleiro
  
 #Inicia assets
 
-assets = {}
-assets['background'] =  pygame.image.load('img/estadio.png').convert() #guarda a imagem do background que aparecerá no jogo
-assets["background"] = pygame.transform.scale(assets["background"], (WIDTH, HEIGHT)) # define a altura e largura do background
-assets['football_img'] =  pygame.image.load('img/football.png').convert_alpha() #guarda a imagem da bola de futebol
-assets['football_img'] = pygame.transform.scale(assets["football_img"], (ball_width, ball_height)) #define a altura e lagura da bola de futebol no jogo
-assets['goalkeeper_img'] = pygame.image.load('img/Goalkeeper.png').convert_alpha()#guarda a imagem do goleiro que aparecerá no jogo
-assets['goalkeeper_img'] = pygame.transform.scale(assets["goalkeeper_img"], (gk_width, gk_height))#define a altura e lagura do goleiro no jogo
-assets['powerup'] = pygame.image.load('img/powerup_shield.png').convert_alpha() #guarda a imagem do powerup "Shield" que aparecerá no jogo
-assets['powerup'] = pygame.transform.scale(assets['powerup'], (ball_width, ball_height)) #define a altura e lagura do powerup "Shield" no jogo
-assets["score_font"] = pygame.font.Font('font/SoccerLeague.ttf', 28) #guarda a fonte da pontuação que aparecerá no jogo
-assets["score_font_2"] = pygame.font.Font('font/PressStart2P.ttf', 28)#guarda a fonte da vida do jogador que aparecerá no jogo
-
+def load_assets():
+    assets = {}
+    assets['background'] =  pygame.image.load('img/estadio.png').convert() #guarda a imagem do background que aparecerá no jogo
+    assets["background"] = pygame.transform.scale(assets["background"], (WIDTH, HEIGHT)) # define a altura e largura do background
+    assets['football_img'] =  pygame.image.load('img/football.png').convert_alpha() #guarda a imagem da bola de futebol
+    assets['football_img'] = pygame.transform.scale(assets["football_img"], (ball_width, ball_height)) #define a altura e lagura da bola de futebol no jogo
+    assets['goalkeeper_img'] = pygame.image.load('img/Goalkeeper.png').convert_alpha()#guarda a imagem do goleiro que aparecerá no jogo
+    assets['goalkeeper_img'] = pygame.transform.scale(assets["goalkeeper_img"], (gk_width, gk_height))#define a altura e lagura do goleiro no jogo
+    assets['powerup'] = pygame.image.load('img/powerup_shield.png').convert_alpha() #guarda a imagem do powerup "Shield" que aparecerá no jogo  
+    assets['powerup'] = pygame.transform.scale(assets['powerup'], (ball_width, ball_height)) #define a altura e lagura do powerup "Shield" no jogo
+    assets["score_font"] = pygame.font.Font('font/SoccerLeague.ttf', 28) #guarda a fonte da pontuação que aparecerá no jogo
+    assets["score_font_2"] = pygame.font.Font('font/PressStart2P.ttf', 28)#guarda a fonte da vida do jogador que aparecerá no jogo
+    return assets
 
 #======================
 #-------Classes-------
@@ -150,155 +151,158 @@ class PowerUp(pygame.sprite.Sprite):
 #-------Loop Principal-------
 #============================
 
+def gamescreen(window):
+    global shield , p_up
+    
+    assets = load_assets()
 
-#Definindo os frames por segundo para ajustar a velocidade da bola
-clock = pygame.time.Clock()
-FPS = 30
+    tempo_p_up = 2000
+    
+    #Definindo os frames por segundo para ajustar a velocidade da bola
+    clock = pygame.time.Clock()
  
-#Criando um grupo bolas
-all_balls = pygame.sprite.Group()
-all_power_ups = pygame.sprite.Group()
-all_soccer_balls = pygame.sprite.Group()
-groups = {}
-groups["all_balls"] = all_balls
-groups["all_soccer_balls"] = all_soccer_balls
-groups["all_power_ups"] = all_power_ups
+    #Criando grupos
+    all_balls = pygame.sprite.Group()
+    all_power_ups = pygame.sprite.Group()
+    all_soccer_balls = pygame.sprite.Group()
+    groups = {}
+    groups["all_balls"] = all_balls
+    groups["all_soccer_balls"] = all_soccer_balls
+    groups["all_power_ups"] = all_power_ups
  
-#Criando o jogador
-player = Gk(assets["goalkeeper_img"])
-all_balls.add(player)
+    #Criando o jogador
+    player = Gk(assets["goalkeeper_img"])
+    all_balls.add(player)
  
-#Criando as bolas
-for i in range(1):
-    balls = Football(assets["football_img"])
-    all_balls.add(balls)
-    all_soccer_balls.add(balls)
- 
+    #Criando as bolas
+    for i in range(1):
+        balls = Football(assets["football_img"])
+        all_balls.add(balls)
+        all_soccer_balls.add(balls)
 
-#Criando o powerup
-powerup = PowerUp(assets['powerup'])
-all_power_ups.add(powerup)
+    #Criando o powerup
+    powerup = PowerUp(assets['powerup'])
+    all_power_ups.add(powerup)
 
-#Estado do jogo
-DONE = 0 #o jogo terminou
-PLAYING = 1 #o jogador está jogando
-state = PLAYING #definindo o estado inicial do jogo como PLAYING
+    #Estado do jogo
+    DONE = 0 #o jogo terminou
+    PLAYING = 1 #o jogador está jogando
+    state = PLAYING #definindo o estado inicial do jogo como PLAYING
 
-#Inicia o placar com 0 pontos
-pontos = 0
+    #Inicia o placar com 0 pontos
+    pontos = 0
 
-#Cria um dicionário de teclas que guarda se alguma tecla estava pressionada ou não
-keys_down = {}
+    #Cria um dicionário de teclas que guarda se alguma tecla estava pressionada ou não
+    keys_down = {}
 
-#Inicia o número total de vidas como 3
-vidas = 3
+    #Inicia o número total de vidas como 3
+    vidas = 3
 
-
-t = pygame.time.get_ticks()
-while state != DONE:
-    time_now = pygame.time.get_ticks()
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            #game = False
-            state = DONE
-        if state == PLAYING:
-            # Verifica se apertou alguma tecla.
-            if event.type == pygame.KEYDOWN:
-                # Dependendo da tecla, altera a velocidade.
-                keys_down[event.key] = True
-                if event.key == pygame.K_LEFT:
-                    player.speedx -= 10
-                if event.key == pygame.K_RIGHT:
-                    player.speedx += 10
-            # Verifica se soltou alguma tecla.
-            if event.type == pygame.KEYUP:
-                # Dependendo da tecla, altera a velocidade.
-                if event.key in keys_down and keys_down[event.key]:
+    t = pygame.time.get_ticks()
+    #Trata eventos
+    while state != DONE:
+        time_now = pygame.time.get_ticks()
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            #Verifica consequencias
+            if event.type == pygame.QUIT:
+                state = DONE
+            if state == PLAYING:
+                # Verifica se apertou alguma tecla.
+                if event.type == pygame.KEYDOWN:
+                    # Dependendo da tecla, altera a velocidade.
+                    keys_down[event.key] = True
                     if event.key == pygame.K_LEFT:
-                        player.speedx += 10
-                    if event.key == pygame.K_RIGHT:
                         player.speedx -= 10
-    #Definindo o intervalo de tempo para o powerup aparecer
-    if time_now-t >= 10000 and p_up == False:
-        p_up = True
-        t = time_now
+                    if event.key == pygame.K_RIGHT:
+                        player.speedx += 10
+             # Verifica se soltou alguma tecla.
+                if event.type == pygame.KEYUP:
+                    # Dependendo da tecla, altera a velocidade.
+                    if event.key in keys_down and keys_down[event.key]:
+                        if event.key == pygame.K_LEFT:
+                            player.speedx += 10
+                        if event.key == pygame.K_RIGHT:
+                            player.speedx -= 10
+        #Definindo o intervalo de tempo para o powerup aparecer
+        if time_now-t >= tempo_p_up and p_up == False:
+            p_up = True
+            t = time_now
     
-            
-
-   
-    #-----------------------------------
-    #---- Atualiza o estado do jogo ----
-    #-----------------------------------
+        #-----------------------------------
+        #---- Atualiza o estado do jogo ----
+        #-----------------------------------
  
-    #Controlando movimentos das 5 bolas no loop
-    all_balls.update()
-    powerup.update()
+        #Controlando movimentos das 5 bolas no loop
+        all_balls.update()
+        powerup.update()
 
-    # Verifica se houve colisão entre o goleiro e a bola
-    if state == PLAYING:
-        #Quando o goleiro pega a bola
-        hits = pygame.sprite.spritecollide(player, all_soccer_balls, True)
-        for colides in hits:
-            x = Football(assets['football_img'])
-            all_balls.add(x)
-            all_soccer_balls.add(x)
-            #Ganha pontos
-            pontos += 10
+        # Verifica se houve colisão entre o goleiro e a bola
+        if state == PLAYING:
+            #Quando o goleiro pega a bola
+            hits = pygame.sprite.spritecollide(player, all_soccer_balls, True)
+            for colides in hits:
+                x = Football(assets['football_img'])
+                all_balls.add(x)
+                all_soccer_balls.add(x)
+                #Ganha pontos
+                pontos += 10
         
-        #Quando o goleiro pega o powerup
-        hits_p_up = pygame.sprite.spritecollide(player,all_power_ups,False)
-        for colides in hits_p_up:
-            shield = True
-            t_shield = pygame.time.get_ticks()
-            p_up = False
-            powerup.update_collide()
-        if shield:
-            time_shield = pygame.time.get_ticks()
-            if time_now - t_shield >= 5000:
-                shield = False
-                print('não esta mais no shield')
+            #Quando o goleiro pega o powerup
+            hits_p_up = pygame.sprite.spritecollide(player,all_power_ups,False)
+            for colides in hits_p_up:
+                shield = True
+                t_shield = pygame.time.get_ticks()
+                p_up = False
+                powerup.update_collide()
+            if shield:
+                if time_now - t_shield >= 5000:
+                    shield = False
+                    print('não esta mais no shield')
         
-        #Quando o goleiro não defende a bola
-        if len(list_lives) == 1:
-            vidas = 2
-        if len(list_lives) == 2:
-            vidas = 1
-        if len(list_lives) == 3:
-            vidas = 0
-        if len(list_lives) == 4:
-            state = DONE #Se o goleiro chegar a 0 vidas o jogo termina
+            #Quando o goleiro não defende a bola
+            if len(list_lives) == 1:
+                vidas = 2
+            if len(list_lives) == 2:
+                vidas = 1
+            if len(list_lives) == 3:
+                vidas = 0
+            if len(list_lives) == 4:
+                state = DONE #Se o goleiro chegar a 0 vidas o jogo termina
 
 
-#=====================================================
-#-------Adicionando imagens para tela principal-------
-#=====================================================
+    #=====================================================
+    #-------Adicionando imagens para tela principal-------
+    #=====================================================
 
-    #Fazendo o background aparecer
-    window.fill((255, 255, 255))
-    window.blit(assets["background"], (0, 0))
-    all_balls.draw(window)
+        #Fazendo o background aparecer
+        window.fill((255, 255, 255))
+        window.blit(assets["background"], (0, 0))
+        all_balls.draw(window)
     
-    #Fazendo o powerup aparecer
-    if p_up:
-        window.blit(assets['powerup'], powerup.rect)
+        #Fazendo o powerup aparecer
+        if p_up:
+            window.blit(assets['powerup'], powerup.rect)
+            print("funcionou")
 
-    # Desenhando o placar
-    text_surface = assets['score_font'].render("{:08d}".format(pontos), True, (255, 255, 255))
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (WIDTH / 6,  HEIGHT- 379)
-    window.blit(text_surface, text_rect)
+        # Desenhando o placar
+        text_surface = assets['score_font'].render("{:08d}".format(pontos), True, (255, 255, 255))
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (WIDTH / 6,  HEIGHT- 379)
+        window.blit(text_surface, text_rect)
 
-    # Desenhando as vidas
-    text_surface = assets['score_font_2'].render(chr(9829) * vidas, True, (255, 0, 0))
-    text_rect = text_surface.get_rect()
-    text_rect.bottomleft = (400, HEIGHT - 350)
-    window.blit(text_surface, text_rect)
+        # Desenhando as vidas
+        text_surface = assets['score_font_2'].render(chr(9829) * vidas, True, (255, 0, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.bottomleft = (400, HEIGHT - 350)
+        window.blit(text_surface, text_rect)
 
-    # Mostra o novo frame para o jogador
-    pygame.display.update() 
+        # Mostra o novo frame para o jogador
+        pygame.display.update() 
 
+#Fim do loop principal
 
+gamescreen(window)
 #=========================
 #-------Finalização-------
 #=========================
